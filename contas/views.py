24f1fdf2fcea1f244a20d29a1cd291b8c0c67fde
Ricart <ricarteusuo11@gmail.com>
2020-03-01 23:13:34 -0300

    implementando criação de model

--- contas/views.py
+++ contas/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transacao
+from .form import TransacaoForm
+
 
 def home(request):
     data = {}
     data['trasacoes'] = Transacao.objects.all()
     return render(request, 'contas/home.html', data)
+
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list')
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import home
+from contas.views import home, nova_transacao
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home, name='list'),
+    path('nova/', nova_transacao, name='new')
 ]
